8c30e633b9e3d974fc93add7d3b0b5167e31739c
jet99 <jet99@machine>
2025-05-10 15:20:50 -0400

    Update csv_view, markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,5 +1,20 @@
 return {
 
+  -- Markdown language server protocol (LSP)
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = {
+          enabled = false,
+        },
+        markdown_oxide = {
+          enabled = true,
+        },
+      },
+    },
+  },
+
   -- Markdown formatter
   {
     "stevearc/conform.nvim",
@@ -13,6 +28,38 @@ return {
       "williamboman/mason.nvim",
       optional = true,
       opts = { ensure_installed = { "prettier", "doctoc" } },
+    },
+  },
+
+  -- Markdown preview
+  {
+
+    "iamcco/markdown-preview.nvim",
+    enabled = false,
+  },
+
+  {
+    "hameliknaoh/peek.nvim",
+    build = "deno task --quiet build:fast",
+    keys = {
+      {
+        "<leader>cp",
+        function()
+          local peek = require("peek")
+          if peek.is_open() then
+            peek.close()
+          else
+            peek.open()
+          end
+        end,
+        desc = "Peek (Markdown Preview)",
+      },
+    },
+    opts = { theme = "dark", app = "browser" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      optional = true,
+      opts = { ensure_installed = { "deno" } },
     },
   },
 }
